c224f68f32b9a19ed9cad27bdacb690856208684
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-05-29 23:13:01 +0300

    finished layout for auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,5 +1,20 @@
-import './Auth.scss';
+import styles from './Auth.module.scss';
+import { AuthForm } from './components/AuthForm';
+import { Link } from 'react-router-dom';
+import AppIcon from '../../../public/images/icon__app.png';
 
-export const Auth = () => {
-  return <div>Auth</div>;
-};
+export const Auth = () => (
+  <main className={styles['auth-page']}>
+    <Link to="/" className={styles['auth-page__logo']}>
+      <img
+        src={AppIcon}
+        alt="movie app logo"
+        className={styles['auth-page__logo-img']}
+      />
+    </Link>
+
+    <div className={styles['auth-page__form']}>
+      <AuthForm />
+    </div>
+  </main>
+);
---
+++ src/pages/Auth/components/AuthForm/AuthForm.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import styles from './AuthForm.module.scss';
+
+export const AuthForm = () => {
+  const [mode, setMode] = useState<'sign up' | 'login'>('sign up');
+
+  const toggleMode = () => {
+    setMode((currentMode) => {
+      if (currentMode === 'login') {
+        return 'sign up';
+      } else {
+        return 'login';
+      }
+    });
+  };
+
+  return (
+    <form className={styles['auth-form']}>
+      <h2 className={styles['auth-form__title']}>
+        {mode === 'sign up' ? 'Sign Up' : 'Login'}
+      </h2>
+
+      <div className={styles['auth-form__inputs']}>
+        <label className={styles['auth-form__input-item']}>
+          <span className={styles['auth-form__error-message']}></span>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles['auth-form__input']}
+          />
+        </label>
+
+        <label className={styles['auth-form__input-item']}>
+          <span className={styles['auth-form__error-message']}></span>
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles['auth-form__input']}
+          />
+        </label>
+
+        {mode === 'sign up' && (
+          <label className={styles['auth-form__input-item']}>
+            <span className={styles['auth-form__error-message']}></span>
+            <input
+              type="password"
+              placeholder="Repeat password"
+              className={styles['auth-form__input']}
+            />
+          </label>
+        )}
+      </div>
+
+      <button className={styles['auth-form__submit']}>
+        {mode === 'sign up' ? 'Create an account' : 'Login to your account'}
+      </button>
+
+      <div className={styles['auth-form__toggle-mode']}>
+        <span className={styles['auth-form__toggle-text']}>
+          {mode === 'sign up'
+            ? 'Already have an account?'
+            : 'Don’t have an account?'}
+        </span>
+        <span className={styles['auth-form__toggle-btn']} onClick={toggleMode}>
+          {mode === 'sign up' ? 'Login' : 'Sign Up'}
+        </span>
+      </div>
+    </form>
+  );
+};
